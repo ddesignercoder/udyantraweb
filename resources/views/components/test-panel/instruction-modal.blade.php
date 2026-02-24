f97e926bb5184940998840cff769c9295c1270dd
PRINCE KUMAR <pk759670@gmail.com>
2026-02-24 13:22:08 +0530

    Update testpanel UI and load Instruction model on loading

--- resources/views/components/test-panel/instruction-modal.blade.php
+++ resources/views/components/test-panel/instruction-modal.blade.php
@@ -55,11 +55,7 @@
 
         <!-- Modal Footer -->
         <div class="bg-gray-50 px-6 py-4 flex gap-3 justify-end">
-            <button 
-                class="px-6 py-2.5 rounded-lg border border-gray-300 bg-white text-gray-700 font-semibold hover:bg-gray-100 transition-colors duration-200">
-                Cancel
-            </button>
-            <button @click="showInstructionModal = false;" class="px-6 py-2.5 rounded-lg bg-linear-to-r from-green-600 to-green-700 text-white font-semibold hover:from-green-700 hover:to-green-800 shadow-lg hover:shadow-xl transition-all duration-200">
+            <button @click="showInstructionModal = false;" class="px-6 py-2.5 rounded-lg bg-linear-to-r from-green-600 to-green-700 text-white font-semibold hover:from-green-700 hover:to-green-800 shadow-lg hover:shadow-xl transition-all duration-200 cursor-pointer">
                 Start Test
             </button>
         </div>
